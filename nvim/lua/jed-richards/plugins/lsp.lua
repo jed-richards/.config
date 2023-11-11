519b3b9982decd6fc59cdca4097c3f765c4b63ff
jed-richards <jed22richards@gmail.com>
2023-11-10 18:15:18 -0600

    lsp

--- nvim/lua/jed-richards/plugins/lsp.lua
+++ nvim/lua/jed-richards/plugins/lsp.lua
@@ -7,6 +7,6 @@ return {
     {'hrsh7th/nvim-cmp'},
     {'hrsh7th/cmp-buffer'},
     {'hrsh7th/cmp-path'},
-    {'L3MON4D3/LuaSnip'},
+    {'L3MON4D3/LuaSnip', build = 'make install_jsregexp'},
     {'rafamadriz/friendly-snippets'},
 }
